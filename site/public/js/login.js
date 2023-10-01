6652575ad72580b99c85409d6cd984f5d4a423e9
KauanMO <kauaanmatheus@gmail.com>
2023-10-01 19:19:04 -0300

    Login pronto

--- site/public/js/login.js
+++ site/public/js/login.js
@@ -1,5 +1,7 @@
 document.querySelector('#login').addEventListener('click', validarCampos)
 
+sessionStorage.clear()
+
 function validarCampos() {
     const email = document.querySelector('#iEmail').value, senha = document.querySelector('#iSenha').value, cookie = document.querySelector('#cookie')
     cookie.innerText = ''
@@ -10,7 +12,18 @@ function validarCampos() {
         fetch(`${window.location.origin}/usuarios/login/${email}/${senha}`, { cache: 'no-store' }).then(res => {
             if (res.ok) {
                 if (res.status == 200) {
-                    console.log('Login realizado');
+                    res.json().then(json => {
+                        if(json[0].status == 1) {
+                            sessionStorage.setItem('idUsuario', json[0].idUsuario)
+                            sessionStorage.setItem('nomeUsuario', json[0].nome)
+                            sessionStorage.setItem('idEmpresa', json[0].fkEmpresa)
+                            setTimeout(() => {
+                                window.location.href = json[0].fkTipoUsuario == 1 ? `${window.location.origin}/dashboards/dashboard-geral.html` : `${window.location.origin}/dashboards/dashboard-lanhouse.html`
+                            }, 500);
+                        } else {
+                            cookie.innerText = 'Usuario bloqueado'
+                        }
+                    })
                 } else {
                     cookie.innerText = 'Nenhum cadastro encontrado'
                 }
