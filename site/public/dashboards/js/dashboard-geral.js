c8f69d8c01d91639b3a7e082e115816935e66d78
KauanMO <kauaanmatheus@gmail.com>
2023-10-05 12:39:14 -0300

    modal de endereco completo em lista de lanhouses

--- site/public/dashboards/js/dashboard-geral.js
+++ site/public/dashboards/js/dashboard-geral.js
@@ -10,7 +10,7 @@ fetch(`${window.location.origin}/lanhouses/listarLanhousesPorEmpresa/${sessionSt
                     <td>${lanhouse.cnpj}</td>
                     <td>${lanhouse.nome}</td>
                     <td>${lanhouse.email}</td>
-                    <td>${lanhouse.logradouro}, ${lanhouse.numero}</td>
+                    <td idEndereco = ${lanhouse.idEndereco} id="endereco-open-modal">${lanhouse.logradouro}, ${lanhouse.numero}</td>
                     <td><div class='status-indicador ${lanhouse.statusLanhouse == 1 ? 'status-ativo' : 'status-bloqueado'}'></div></td>
                     <td><span idLanhouse='${lanhouse.idLanHouse}' class='login-direto'>Fazer login</span></td>
                     <td><span statusLanhouse=${lanhouse.statusLanhouse} idLanhouse='${lanhouse.idLanHouse}' class='ativar-desativar-lanhouse'>${lanhouse.statusLanhouse == 1 ? 'Desativar' : 'Ativar'}</span></td>
@@ -26,7 +26,33 @@ setTimeout(() => {
     document.querySelectorAll('.ativar-desativar-lanhouse').forEach(lanhouse => {
         lanhouse.addEventListener('click', desativarOuAtivarLanhouse);
     })
+
+    $(document).ready(() => {
+        $('#endereco-open-modal').on('click', (e) => {
+            buscarEndereco(e.target.getAttribute('idEndereco'))
+            $('#modal-endereco-lanhouse').modal('show')
+        })
+    })
 }, 200);
+
+const buscarEndereco = idEndereco => {
+    fetch(`${window.location.origin}/enderecos/buscarEnderecoPorId/${idEndereco}`, { cache: "no-cache" }).then(res => {
+        if (res.ok) {
+            let modalEndereco = document.querySelector('#modal-endereco-lanhouse')
+            res.json().then(endereco => {
+                modalEndereco.querySelector('#idEnderecoModal').innerText = endereco[0].idEndereco
+                modalEndereco.querySelector('#cepModal').innerText = endereco[0].cep
+                modalEndereco.querySelector('#ufModal').innerText = endereco[0].uf
+                modalEndereco.querySelector('#cidadeModal').innerText = endereco[0].cidade
+                modalEndereco.querySelector('#bairroModal').innerText = endereco[0].bairro
+                modalEndereco.querySelector('#logradouroModal').innerText = endereco[0].logradouro
+                modalEndereco.querySelector('#numeroModal').innerText = endereco[0].numero
+            })
+        } else {
+            console.log('Erro na busca de endereco desta lanhouse')
+        }
+    })
+}
 
 const desativarOuAtivarLanhouse = e => {
     fetch(`${window.location.origin}/lanhouses/${e.target.getAttribute('statusLanhouse') == 1 ? 'desativar' : 'ativar'}/${e.target.getAttribute('idLanhouse')}`, {
