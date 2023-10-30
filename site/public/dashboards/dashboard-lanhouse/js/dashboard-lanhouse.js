262753080686aa8d14a37274f09690b897e3c08e
KauanMO <kauaanmatheus@gmail.com>
2023-10-30 17:11:04 -0300

    Apresentação de código de lanhouse

--- site/public/dashboards/dashboard-lanhouse/js/dashboard-lanhouse.js
+++ site/public/dashboards/dashboard-lanhouse/js/dashboard-lanhouse.js
@@ -1,14 +1,15 @@
-console.log(sessionStorage)
-
 fetch(`${window.location.origin}/lanhouses/buscarLanHousePorId/${sessionStorage.getItem('idLanhouse')}`, { cache: "no-cache" }).then(res => {
     if (res.ok) {
         res.json().then(lanhouse => {
             printLanhouse(lanhouse[0].unidade)
+            document.querySelector('#insert-codigo-lanhouse').innerText = lanhouse[0].codigoAcesso
         })
     } else {
         console.log('Erro na busca da lanhouse')
     }
 })
+
+
 
 document.querySelectorAll('.print-username').forEach(usernameClass => {
     usernameClass.innerText = sessionStorage.getItem('nomeUsuario')
