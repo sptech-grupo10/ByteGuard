231017a4c0b2ffba59ec09c68b21e18d16dd0835
KauanMO <kauaanmatheus@gmail.com>
2023-11-12 12:29:11 -0300

    Buscando maquinas e componentes sobrecarregados

--- site/public/dashboards/dashboard-lanhouse/js/index.js
+++ site/public/dashboards/dashboard-lanhouse/js/index.js
@@ -1,16 +1,18 @@
 listarMaquinas()
 
 async function listarMaquinas() {
-    res = await fetch(`/maquinas/buscarMaquinasPorLanHouse/${sessionStorage.getItem('idLanhouse')}`)
+    res = await fetch(`/maquinas/buscarMaquinasComponentesForaIdeal/${sessionStorage.getItem('idLanhouse')}`)
     maquina = await res.json()
 
     maquina.forEach(maquina => {
+        let status = maquina.componentessobrecarrecados < 1 ? 'ideal' : 'atencao'
         document.querySelector('.status-maquinas').innerHTML += `
         <div class="box-maquina">
             <div class="status-icon-nome">
-                <div class="cor-legend circle-critico"></div>
+                <div class="cor-legend circle-${status}"></div>
                 <img src="../../assets/imgs/pc-icon-black.png" alt="">
                 <h1>${maquina.nomeMaquina}</h1>
+                <span>Componentes sobrecarregados: ${maquina.componentessobrecarrecados}</span>
             </div>
 
             <div class="botao-acessar">
--- site/src/models/maquinaModel.js
+++ site/src/models/maquinaModel.js
@@ -1,18 +1,24 @@
 const database = require('../database/config');
 
-function buscarMaquinasDaLanHouse(fkEmpresa){
-    try{
+function buscarMaquinasDaLanHouse(fkEmpresa) {
+    try {
         return database.exec(`select * from maquina where fkLanhouse = ${fkEmpresa}`)
-    } catch(e){
+    } catch (e) {
         console.log(e);
     }
 }
 
 function buscarMaquinasComponentesForaIdeal(fkLanhouse) {
-    try{
-        return database.exec(`SELECT count(distinct(fkComponente)) as contagem, maquina.nomeMaquina FROM log JOIN componente ON fkComponente = idComponente 
-        JOIN maquina on idMaquina = fkMaquina WHERE statusLog != 1 AND fkLanhouse = ${fkLanhouse} GROUP BY nomeMaquina ORDER BY contagem;`)
-    }catch(e) {
+    try {
+        return database.exec(`select m.nomeMaquina, count(c.idComponente) as 'componentessobrecarrecados' from log l
+        join componente c on l.fkComponente = c.idComponente
+        join maquina m on c.fkMaquina = m.idMaquina
+        where dataLog = (select dataLog from log order by dataLog desc limit 1)
+        and fkLanhouse = ${fkLanhouse}
+        and l.statusLog != 1
+        group by nomeMaquina
+        order by 'componentessobrecarrecados' desc`)
+    } catch (e) {
         console.log(e)
     }
 }
